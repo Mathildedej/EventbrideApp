370dd42d8a4e55a7c6a2e70c4ec842d0fd295e9b
Mathildedej <mathilde.dejaureguiberry@gmail.com>
2020-05-12 10:48:36 +0200

    Revert "Revert "Revert "pepe"""
    This reverts commit 1060bb648ee8c8136f22f8ba1b7798b9be6dbf19.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,10 @@
 class Event < ApplicationRecord
-  #validates :start_date, presence: true
-  #validates :duration, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-  #validates :title, presence: true, length: { in: 5..140 }
-  #validates :description, presence: true, length: { in: 20..1000 }
-  #validates :price, presence: true, :inclusion => 1..1000
-  #validates :location, presence: true 
-  
+  validates :start_date, presence: true
+  validates :duration, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, :inclusion => 1..1000
+  validates :location, presence: true
   #validates :start_must_be_in_the_futur
   #def start_must_be_in_the_futur
    # errors.add(:start_date, "must be in the futur") unless
