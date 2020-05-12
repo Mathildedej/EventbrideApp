14bf030ffd18cea765e576267637b2659317cd68
Mathildedej <mathilde.dejaureguiberry@gmail.com>
2020-05-12 16:04:27 +0200

    resolving deconnexion problems 1

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   # pour regler le probleme de get quand je fais sign_out
-  devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  #devise_scope :user do
+  #get '/users/sign_out' => 'devise/sessions#destroy'
+  #end
 
 end
